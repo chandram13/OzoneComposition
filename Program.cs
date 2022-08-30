e861e28cc88a1f8cf7448491c64eb78b723146a8
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-30 08:46:19 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿//  Marvish Chandra
+
+
+namespace OzoneComposition
+{
+    class OzoneElements
+    {
+        public static void Atmosphere(string[] args)
+        {
+            constituent = String;
+            molecularMass = Int;
+            mixingRatio = Int;
+            atmosphereRole = String;
+
+            {
+                constituent1 = "nitrogen";
+                constituent1.molecularMass = 28.013;
+                constituent1.mixingRatio = 0.7808;
+                constituent1.role = "transparent; provides heat capacity and momentum; exchanged with biomass; decomposed in combustion";
+                    Console.ReadLine("The first composing element is " + constituent1 + "with the molecular mass of " + constituent1.molecularMass + "and the mixing ratio of" + constituent1.mixingRatio + "." + "The role in the atmosphere is: " + constituent1.role + ".");
+                constituent2 = "oxygen";
+                constituent2.molecularMass = 31.998;
+                constituent2.mixingRatio = 0.2095;
+                constituent2.role = "transparent except for in extreme ultraviolet; provides some heat capacity and momentum; exchanged with life; source of important reactive gases like ozone";
+                    Console.ReadLine("The second composing element is " + constituent2 + "with the molecular mass of " + constituent2.molecularMass + "and the mixing ratio of" + constituent2.mixingRatio + "." + "The role in the atmosphere is: " + constituent2.role + ".");
+                constituent3 = "argon";
+                constituent3.molecularMass = 39.948;
+                constituent3.mixingRatio = 0.0093;
+                constituent3.role = "none";
+                    Console.ReadLine("The third composing element is " + constituent3 + "with the molecular mass of " + constituent3.molecularMass + "and the mixing ratio of" + constituent3.mixingRatio + "." + "The role in the atmosphere is: " + constituent3.role + ".");
+                constituent4 = "carbon dioxide";
+                constituent4.molecularMass = 44.010;
+                constituent4.mixingRatio = 0.000385;
+                constituent4.role = "transparent in visible; absorbs infrared light (i.e., contributes to global warming); exchanged with life; product of combustion";
+                    Console.ReadLine("The fourth composing element is " + constituent4 + "with the molecular mass of " + constituent4.molecularMass + "and the mixing ratio of" + constituent4.mixingRatio + "." + "The role in the atmosphere is: " + constituent4.role + ".");
+                constituent5 = "neon";
+                constituent5.molecularMass = 20.183;
+                constituent5.mixingRatio = 0.0000182;
+                constituent5.role = "no role, but makes colorful glowing signs";
+                    Console.ReadLine("The fifth composing element is " + constituent5 + "with the molecular mass of " + constituent5.molecularMass + "and the mixing ratio of" + constituent5.mixingRatio + "." + "The role in the atmosphere is: " + constituent5.role + ".");
+                constituent6 = "water vapor";
+                constituent6.molecularMass = 18.015;
+                constituent6.mixingRatio = 0.05;
+                constituent6.role = "gas transparent in visible; absorbs infrared light (i.e., contributes to global warming); exists as vapor, liquid, and solid; exchanged with life; product of combustion";
+                    Console.ReadLine("The sixth composing element is " + constituent6 + "with the molecular mass of " + constituent6.molecularMass + "and the mixing ratio of" + constituent6.mixingRatio + "." + "The role in the atmosphere is: " + constituent6.role + ".");
+                constituent7 = "aerosol particules";
+                constituent7.molecularMass = 0;
+                constituent7.mixingRatio = 500;
+                constituent7.role = "essential for cloud formation; interact with visible and infrared light; exchanged with surfaces and life";
+                    Console.ReadLine("The seventh composing element is " + constituent7 + "with the molecular mass of " + constituent7.molecularMass + "and the mixing ratio of" + constituent7.mixingRatio + "." + "The role in the atmosphere is: " + constituent7.role + ".");
+                constituent8 = "methane";
+                constituent8.molecularMass = 16.04;
+                constituent8.mixingRatio = 0.00000182;
+                constituent8.role = "transparent in visible; absorbs in infrared (i.e. contributes to global warming); exchanged with life; source of CO2 and H2O";
+                    Console.ReadLine("The eighth composing element is " + constituent8 + "with the molecular mass of " + constituent8.molecularMass + "and the mixing ratio of" + constituent8.mixingRatio + "." + "The role in the atmosphere is: " + constituent8.role + ".");
+                constituent9 = "ozone";
+                constituent9.molecularMass = 48.00;
+                constituent9.mixingRatio = 0.01;
+                constituent9.role = "transparent in visible; absorbs in UV and infrared; reactive and source of more reactive gases";
+                    Console.ReadLine("The ninth composing element is " + constituent9 + "with the molecular mass of " + constituent9.molecularMass + "and the mixing ratio of" + constituent9.mixingRatio + "." + "The role in the atmosphere is: " + constituent9.role + ".");
+                constituent10 = "particules";
+                constituent10.molecularMass = 0;
+                constituent10.mixingRatio = 100;
+                constituent10.role = "absorbs and scatters light; acts as CCN and IN";
+                    Console.ReadLine("The tenth composing element is " + constituent10 + "with the molecular mass of " + constituent10.molecularMass + "and the mixing ratio of" + constituent10.mixingRatio + "." + "The role in the atmosphere is: " + constituent10.role + ".");
+                    
+            }
+        }
+    }
+}
